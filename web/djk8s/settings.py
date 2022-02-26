5152fbd1c8c9d8ce17d712f789a8493dc911444f
Bhuwan Chaudhary <bhuwanchy069@gmail.com>
2022-02-26 17:23:04 +0545

    Updated k8s del django  secrets

--- web/djk8s/settings.py
+++ web/djk8s/settings.py
@@ -16,12 +16,12 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for production 
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-+czfjthr#yk9-(+-blz6j0umot)pyjd!s@ezhn@)ft)h(ohhpg'
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = 'django-insecure-+czfjthffdsdr#yk9-(+-blz6j0umot)pyjd!s@ezhn@)ft)h(ohhpg'
+# SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1'
 
